6c29c3cf154bd96f6524d904bfc8894aeaacfb1f
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-01-10 04:36:20 -0500

    Update

--- snake/model.py
+++ snake/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import numpy as np
 import os
 
 MODEL_DIR = './model'
@@ -50,7 +51,8 @@ class QTrainer:
     self.criterion = nn.MSELoss()
     
   def train_step(self, state, action, reward, next_state, game_over):
-    state = torch.tensor(state, dtype=torch.float)
+    np_state = np.array(state)
+    state = torch.tensor(np_state, dtype=torch.float)
     next_state = torch.tensor(next_state, dtype=torch.float)
     action = torch.tensor(action, dtype=torch.long)
     reward = torch.tensor(reward, dtype=torch.float)
