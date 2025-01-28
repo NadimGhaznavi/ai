1171177cb49a755edfcedb4aec96225b3c26ec0f
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-01-28 18:02:51 -0500

    Fixed bug in the plot function: Horizontal dividers now working

--- snake/SnakeGamePlots.py
+++ snake/SnakeGamePlots.py
@@ -28,42 +28,44 @@ class MyPlot():
     plt.title('Snake AI Training (v' + str(self.ini.get('ai_version')) + ')')
     plt.xlabel('Number of games')
     plt.ylabel('Score')
+
+    # Create a games array for the x-axis
+    games = []
+    game_num = 1
+    for x in scores:
+      games.append(game_num)
+      game_num += 1
+    np_games =np.array(games)
+
     # Plot horizontal lines to differentiate the ranges of scores into groups of 10
     # Only plot the horizontal lines if there is a score that is equal to or greater than 10
     for y in [10, 20, 30, 40, 50, 60, 70, 80, 90]:
-      if max(scores) > y:
+      if max(scores) >= y:
         plt.axhline(y=y, color='r', linestyle=':')
     
     # Plot vertical lines to break the results into comparable sections
-    if max(scores) > 100 and max(scores) <= 200:
+    if max(games) < 200:
       for y in [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]:
-        if max(scores) >= y:
+        if max(games) >= y:
           plt.axvline(x=y, color='r', linestyle=':')
-    elif max(scores) > 200 and max(scores) <= 400:
+    elif max(games) > 200 and max(games) <= 400:
       for y in [40, 80, 120, 160, 200, 240, 280, 320, 360, 400]:
-        if max(scores) >= y:
+        if max(games) >= y:
           plt.axvline(x=y, color='r', linestyle=':')
-    elif max(scores) > 400 and max(scores) <= 800:
+    elif max(games) > 400 and max(games) <= 800:
       for y in [80, 160, 240, 320, 400, 480, 560, 640, 720, 800]:
-        if max(scores) >= y:
+        if max(games) >= y:
           plt.axvline(x=y, color='r', linestyle=':')
     else:
       for y in [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000,
                 2200, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000]:
-        if max(scores) >= y:
+        if max(games) >= y:
           plt.axvline(x=y, color='r', linestyle=':')
 
     # Smooth the scores into a nice curve if we have more than 10
     if len(scores) > 10:
       np_scores = np.array(scores)
       
-      # Create a games array to represent the game number
-      games = []
-      game_num = 1
-      for x in scores:
-        games.append(game_num)
-        game_num += 1
-      np_games =np.array(games)
 
       X_Y_Spline = make_interp_spline(np_games, np_scores)
       X = np.linspace(np_games.min(), np_games.max(), 50*len(games))
